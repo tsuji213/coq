ec892dea2f9f1f3b119209646504bd5af78696d5
tsuji213 <kost.ynu@gmail.com>
2013-06-14 12:28:54 +0900

    commit

--- practice.v
+++ practice.v
@@ -1,33 +1,7 @@
+
 Require Import SfLib.
 Require Import Logic.
 Require Import Basics.
-Require Import Datatypes.
-
-Inductive id : Type :=
-  Id : nat -> id.
-
-Fixpoint beq_nat n m : bool :=
-  match n, m with
-    | O, O => true
-    | O, S _ => false
-    | S _, O => false
-    | S n1, S m1 => beq_nat n1 m1
-  end.
-
-Definition beq_id id1 id2 :=
-  match (id1, id2) with
-    (Id n1, Id n2) => beq_nat n1 n2
-  end.
-
-Lemma beq_nat_refl : forall n, true = beq_nat n n.
-Proof.
- Admitted.
-
-Theorem beq_id_refl : forall i,
-  true = beq_id i i.
-Proof.
-  intros. destruct i.
-  apply beq_nat_refl. Qed.
 
 Inductive ty : Type :=
   | ty_Bool : ty
@@ -46,46 +20,20 @@ Tactic Notation "tm_cases" tactic(first) ident(c) :=
   [ Case_aux c "tm_var" | Case_aux c "tm_app"
   | Case_aux c "tm_abs" | Case_aux c "tm_true"
   | Case_aux c "tm_false" | Case_aux c "tm_if" ].
-(*
+
+Definition context := partial_map ty.
+
 Inductive option (A:Type) : Type :=
   | Some : A -> option A
   | None : option A.
 
-Implicit Arguments None [A].
-
-Definition option_map (A B:Type) (f:A->B) o :=
+(*
+Definition option_map (A B:Type) (f:A->B) o (a:A):=
   match o with
     | Some a => Some (f a)
     | None => None
   end.
 *)
-Definition context := partial_map ty.
-(*
-Module Context.
-
-Definition partial_map (A:Type) := id -> option A.
-
-Definition empty {A:Type} : partial_map A := (fun _ => None).
-
-Definition extend {A:Type} (Γ : partial_map A) (x:id) (T : A) :=
-  fun x' => if beq_id x x' then Some T else Γ x'.
-
-Lemma extend_eq : forall A (ctxt: partial_map A) x T,
-  (extend ctxt x T) x = Some T.
-Proof.
-  intros. unfold extend. rewrite <- beq_id_refl. auto.
-Qed.
-
-Lemma extend_neq : forall A (ctxt: partial_map A) x1 T x2,
-  beq_id x2 x1 = false ->
-  (extend ctxt x2 T) x1 = ctxt x1.
-Proof.
-  intros. unfold extend. rewrite H. auto.
-Qed.
-
-End Context.
-*)
-
 
 Inductive has_type : context -> tm -> ty -> Prop :=
   | T_Var : forall (Γ:context) (x: id) (T : ty),
